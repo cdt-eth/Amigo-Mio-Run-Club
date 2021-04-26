a935891592df225e7397473ac5f2587da3c64ba0
christianturner <christiandavidphoto@gmail.com>
2021-04-26 00:16:06 -0400

    contact page

--- pages/contact.js
+++ pages/contact.js
@@ -2,25 +2,46 @@ import Head from "next/head";
 import styled from "styled-components";
 
 const Hero = styled.div`
-  height: 81vh;
+  height: 55vh;
   display: flex;
   justify-content: center;
   align-items: center;
   background: #fff;
-
+  margin: 20px;
+  /* border: 1px solid red; */
   @media only screen and (max-width: 768px) {
-    height: 80vh;
+    height: 55vh;
   }
 `;
 
 const Heading = styled.div`
   color: #000;
   font-weight: 900;
-  font-size: 5rem;
+  font-size: 8rem;
+  padding-right: 25px;
+  margin-bottom: 50px;
 
-  @media only screen and (min-width: 768px) {
-    font-size: 10rem;
+  @media only screen and (max-width: 768px) {
+    font-size: 4rem;
   }
+`;
+
+const Body = styled.p`
+  color: #000;
+  font-weight: 900;
+  font-size: 5rem;
+  padding-right: 25px;
+  margin: 20px;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 1.3rem;
+    /* line-height: 2.6rem; */
+  }
+`;
+
+const Accent = styled.span`
+  color: orange !important;
+  font-style: italic;
 `;
 
 export default function Contact() {
@@ -30,10 +51,51 @@ export default function Contact() {
         <title>AMRC: Contact</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Hero>
-        <Heading>Contact</Heading>
+        <Heading>
+          <Accent>CONTACT</Accent> AMRC
+        </Heading>
       </Hero>
+
+      <Body>
+        <div className="intro">
+          <div className="introItem">
+            <p>
+              Strava <span>@</span>
+            </p>
+            <a
+              href="https://www.strava.com/clubs/amigomio"
+              rel="noreferrer"
+              target="_blank"
+            >
+              amigomio{" "}
+            </a>
+          </div>
+          <div className="introItem">
+            <p>
+              Insta <span>@</span>
+            </p>
+            <a
+              href="https://www.instagram.com/amigomiorunclub/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              amigomiorunclub
+            </a>
+          </div>
+          <div className="introItem">
+            <p>
+              Gmail <span>@</span>
+            </p>
+            <a href="mailto:amigomiorunclub@gmail.com">amigomiorunclub</a>
+          </div>
+        </div>
+
+        <br />
+        <br />
+
+        <Accent>#ConTodo</Accent>
+      </Body>
     </>
   );
 }
