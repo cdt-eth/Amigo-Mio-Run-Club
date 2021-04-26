4dce3c1bd2c1289a4d06f4f1d1e73f82d620eeea
christianturner <christiandavidphoto@gmail.com>
2021-04-25 23:37:29 -0400

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -3,20 +3,36 @@ import styled from "styled-components";
 const FooterSection = styled.div`
   background: #000;
   color: #fff;
-  height: 60px;
+  height: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-style: italic;
+  /* font-style: italic; */
   display: flex;
-  min-height: 10vh;
+  min-height: 15vh;
   flex-direction: column;
+
+  @media only screen and (max-width: 768px) {
+    padding: 10px;
+    text-align: center;
+    line-height: 1.5rem;
+  }
 `;
 
 const Footer = () => {
   return (
     <FooterSection>
-      <p>Amigo Mio Run Club, Ltd. • 2021 • All Rights Reserved.</p>
+      <a
+        href="https://www.instagram.com/amigomiorunclub/"
+        style={{ marginTop: 15 }}
+        target="_blank"
+      >
+        <img src="ig.png" style={{ height: 25 }} alt="ig-logo" />
+      </a>{" "}
+      <p style={{ marginTop: 10 }}>
+        {/* Amigo Mio Run Club, Ltd. • 2021 • All Rights Reserved. •{"  "} */}
+        <b>Amigo Mio Run Club,</b> <em>All Rights Reserved</em>
+      </p>
     </FooterSection>
   );
 };
